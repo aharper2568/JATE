a72784194563cc00eb301d61bc947c90a0c17838
aharper2568 <anthonyharper568@gmail.com>
2024-09-16 17:48:58 -0400

    added install feature according to mozilla documentation

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,23 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let installPrompt
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+installPrompt = event;
+butInstall.removeAttribute('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  if (!installPrompt) {
+    return;
+  }
+const result = await installPrompt.prompt();
+butInstall.setAttribute('hidden','');
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  installPrompt = null;
+});
